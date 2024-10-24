254b31ae6939246399a660cff0d45de649db18d1
unknown <sr0589071@gmail.com>
2024-10-24 07:09:58 +0600

    added an bookmarks,readingTime file

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,14 +1,48 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    console.log(blog)
+import { FaBookmark } from 'react-icons/fa';
+const Blog = ({ blog, handleAddBookmarks, handleMarkAsRead }) => {
+    const {title, cover,author_name, author_img,post_date ,reading_time, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8' src={cover}
+             alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4 '>
+                    <div className='flex'>
+                        <img className='w-14' src={author_img} alt="" />
+                        <div className='ml-6'>
+                            <h3 className='text-2xl'>{author_name}</h3>
+                            <p>{post_date}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <span>{reading_time} min read</span>
+
+                        <button onClick={() =>  handleAddBookmarks(blog)} 
+                        className='ml-2 tex-2xl text-red-600'><FaBookmark></FaBookmark>
+                         </button>
+                    </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash ,idx) =>
+                         <span key={idx}><a 
+                    href="">#{hash}</a>
+                    </span>)
+
+
+                }
+            </p>
+            <button 
+            onClick={() =>handleMarkAsRead(reading_time)}
+            className='text-purple-800 font-bold underline'>Mark as read</button>
         </div>
     );
 };
 
 Blog.PropTypes ={
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddBookmarks: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 }
-export default blog;
+export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
-const Blogs = () => {
+import Blog from "../blog/blog";
+import { PropTypes } from 'prop-types';
+const Blogs = ({ handleAddBookmarks, handleMarkAsRead}) => {
 
     const[blogs,setBlogs] = useState([])
 
@@ -13,8 +14,20 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map( blog => 
+                <Blog key={blog.id} 
+                blog={blog}
+                handleAddBookmarks ={handleAddBookmarks}
+                handleMarkAsRead={handleMarkAsRead}
+                ></Blog>)
+            }
         </div>
     );
 };
 
+Blogs.PropTypes = {
+    handleAddBookmarks: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
+}
 export default Blogs;
